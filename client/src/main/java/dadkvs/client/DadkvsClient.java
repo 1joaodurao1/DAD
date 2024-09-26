7278add36b596fb9c01e3a70b980eb8e54bea901
Miguel Fernandes <miguel.e.fernandes@tecnico.ulisboa.pt>
2024-09-26 16:54:24 +0100

    Fix the remainning identation errors

--- client/src/main/java/dadkvs/client/DadkvsClient.java
+++ client/src/main/java/dadkvs/client/DadkvsClient.java
@@ -137,142 +137,146 @@ public class DadkvsClient {
 
 		while (counter < loop_size) {
 			int write_key = rnd.nextInt(key_range)+1;
-		int write_value = rnd.nextInt(1000);
-		
+			int write_value = rnd.nextInt(1000);
+
 			// read key 1
-		int read_key1 = rnd.nextInt(key_range)+1;
-		VersionedValue kv_entry1 = doRead (read_key1);
-		if (kv_entry1 == null) {
-		System.out.println("Panic! ..");
-		return;
-		}
-		Thread.sleep(rnd.nextInt(sleep_range) * 1000);
+			int read_key1 = rnd.nextInt(key_range)+1;
+			VersionedValue kv_entry1 = doRead (read_key1);
+			if (kv_entry1 == null) {
+				System.out.println("Panic! ..");
+				return;
+			}
+			Thread.sleep(rnd.nextInt(sleep_range) * 1000);
 
 			// read key 2
-		int read_key2 = rnd.nextInt(key_range)+1;
-		VersionedValue kv_entry2 = doRead (read_key2);
-		if (kv_entry2 == null) {
-		System.out.println("Panic! ..");
-		return;
-		}
-		Thread.sleep(rnd.nextInt(sleep_range) * 1000);
+			int read_key2 = rnd.nextInt(key_range)+1;
+			VersionedValue kv_entry2 = doRead (read_key2);
+			if (kv_entry2 == null) {
+				System.out.println("Panic! ..");
+				return;
+			}
+			Thread.sleep(rnd.nextInt(sleep_range) * 1000);
 
 
-		System.out.println("Commiting transaction number " + (counter+1));
-		if (doCommit (read_key1, kv_entry1.getVersion(), read_key2, kv_entry2.getVersion(), write_key, write_value))
-		committed++;
+			System.out.println("Commiting transaction number " + (counter+1));
+			if (doCommit (read_key1, kv_entry1.getVersion(), read_key2, kv_entry2.getVersion(), write_key, write_value)){
+				committed++;
+			}
 			Thread.sleep(rnd.nextInt(sleep_range) * 1000);
 			counter++;
 		}
 		System.out.println("loop done. transactions committed = " + committed + ". transactions aborted = " + (loop_size-committed) + ".");
-		
     }
-    
+
     private void initComms () {
-	// Let us use plaintext communication because we do not have certificates
-	channels = new ManagedChannel[n_servers];
+		// Let us use plaintext communication because we do not have certificates
+		channels = new ManagedChannel[n_servers];
 
-	for (int i = 0; i < n_servers; i++) {
-	    channels[i] = ManagedChannelBuilder.forTarget(targets[i]).usePlaintext().build();
-	}
-	
-	async_stubs = new DadkvsMainServiceGrpc.DadkvsMainServiceStub[n_servers];
+		for (int i = 0; i < n_servers; i++) {
+			channels[i] = ManagedChannelBuilder.forTarget(targets[i]).usePlaintext().build();
+		}
 
-	for (int i = 0; i < n_servers; i++) {
-	    async_stubs[i] = DadkvsMainServiceGrpc.newStub(channels[i]);
-	}
+		async_stubs = new DadkvsMainServiceGrpc.DadkvsMainServiceStub[n_servers];
+
+		for (int i = 0; i < n_servers; i++) {
+			async_stubs[i] = DadkvsMainServiceGrpc.newStub(channels[i]);
+		}
     }
 
     private void terminateComms () {
-	for (int i = 0; i < n_servers; i++) {
-	    channels[i].shutdownNow();
-	}
+		for (int i = 0; i < n_servers; i++) {
+			channels[i].shutdownNow();
+		}
     }
 
     public void parseArgs (String[] args) {
-	int length = args.length;
-	int cursor = 0;
-	
-	if (length < 1) {
-	    System.err.println("Argument(s) missing!");
-	    System.err.printf("Usage: java %s clientid [options]%n", DadkvsClient.class.getName());
-	}
-	else {
-	    int id = Integer.parseInt(args[0]);
-	    if (id == 0)
-		System.err.println("Client id=0 reserved for console, using client id=1!");
-	    else
-		client_id = id;
-	}
+		int length = args.length;
+		int cursor = 0;
 
-	cursor = 1;
-	while (cursor < length) {
-	    String option = args[cursor];
-	    String[] option_parts = option.split(" ");
-            String option_name = option_parts[0].toLowerCase();
-            String option_parameter = option_parts.length > 1 ? option_parts[1] : null;
+		if (length < 1) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s clientid [options]%n", DadkvsClient.class.getName());
+		} else {
+			int id = Integer.parseInt(args[0]);
+			if (id == 0)
+			System.err.println("Client id=0 reserved for console, using client id=1!");
+			else
+			client_id = id;
+		}
 
-	    switch (option_name) {
-	        case "--help":
-                    System.out.printf("--help");
-		    System.out.printf("--host hostname");  
-		    System.out.printf("--port portnumber");  
-		    System.out.printf("--range keyrange");  
-		    System.out.printf("--lenght looplenght");  
- 		    System.out.printf("--sleep sleeprange");  
- 		    System.out.printf("-i (iterative mode)");
-		    cursor++;
-		    break;
-		case "--port":
-		    if (option_parameter==null)
-			System.err.println("missing portnumber");
-		    else 
-			port = Integer.parseInt(option_parameter);
-		    break;
-		case "--host":
-		    if (option_parameter==null)
-			System.err.println("missing hostname");
-		    else 
-			host = option_parameter;
-		    break;
-		case "--range":
-		    if (option_parameter==null)
-			System.err.println("missing keyrange");
-		    else 
-			key_range = Integer.parseInt(option_parameter);
-		    break;
-		case "--lenght":
-		    if (option_parameter==null)
-			System.err.println("missing looplenght");
-		    else 
-			loop_size = Integer.parseInt(option_parameter);
-		    break;
-		case "--sleep":
-		    if (option_parameter==null)
-			System.err.println("missing sleeprange");
-		    else 
-			sleep_range= Integer.parseInt(option_parameter);
-		    break;
-		case "-i":
-		    interactive_mode = true;
-		    break;
-	        default:
-		    System.err.println("Unknown option");
-		    break;
-	     }
-	    cursor++;
-	}
+		cursor = 1;
+		while (cursor < length) {
+			String option = args[cursor];
+			String[] option_parts = option.split(" ");
+			String option_name = option_parts[0].toLowerCase();
+			String option_parameter = option_parts.length > 1 ? option_parts[1] : null;
+
+			switch (option_name) {
+				case "--help":
+					System.out.printf("--help");
+					System.out.printf("--host hostname");
+					System.out.printf("--port portnumber");
+					System.out.printf("--range keyrange");
+					System.out.printf("--lenght looplenght");
+					System.out.printf("--sleep sleeprange");
+					System.out.printf("-i (iterative mode)");
+					cursor++;
+					break;
+				case "--port":
+					if (option_parameter==null) {
+						System.err.println("missing portnumber");
+					} else {
+						port = Integer.parseInt(option_parameter);
+					}
+					break;
+				case "--host":
+					if (option_parameter==null) {
+						System.err.println("missing hostname");
+					} else {
+						host = option_parameter;
+					}
+					break;
+				case "--range":
+					if (option_parameter==null) {
+						System.err.println("missing keyrange");
+					} else {
+						key_range = Integer.parseInt(option_parameter);
+					}
+					break;
+				case "--lenght":
+					if (option_parameter==null) {
+						System.err.println("missing looplenght");
+					} else {
+						loop_size = Integer.parseInt(option_parameter);
+					}
+					break;
+				case "--sleep":
+					if (option_parameter==null){
+						System.err.println("missing sleeprange");
+					} else {
+						sleep_range= Integer.parseInt(option_parameter);
+					}
+					break;
+				case "-i":
+					interactive_mode = true;
+					break;
+				default:
+					System.err.println("Unknown option");
+					break;
+			}
+			cursor++;
+		}
     }
 
     public void goInteractive() {
 
 		System.out.println("Client Start. For the full command list, write \"help\"");
 
-	Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
         String command;
 
-	boolean keep_going = true;
-        
+		boolean keep_going = true;
+
         while (keep_going) {
             System.out.print("client> ");
             command = scanner.nextLine();
@@ -283,158 +287,157 @@ public class DadkvsClient {
             String parameter3 = commandParts.length > 3 ? commandParts[3] : null;
 
             switch (mainCommand) {
-	        case "help":
+	        	case "help":
                     System.out.println("\thelp");
                     System.out.println("\tread key");
-		    System.out.println("\ttx read_key read_key write_key");
-		    System.out.println("\tloop");
-		    System.out.println("\trange key-range");
-		    System.out.println("\tlenght loop-lenght");
-		    System.out.println("\ttime sleep-range");
-		    System.out.println("\texit");
-                    break;
+					System.out.println("\ttx read_key read_key write_key");
+					System.out.println("\tloop");
+					System.out.println("\trange key-range");
+					System.out.println("\tlenght loop-lenght");
+					System.out.println("\ttime sleep-range");
+					System.out.println("\texit");
+					break;
                 case "read":
-		    System.out.println("read " + parameter1);
+		    		System.out.println("read " + parameter1);
                     if (parameter1 != null) {
-			try {
-			    int key =  Integer.parseInt(parameter1);
-			    VersionedValue kv_entry  = doRead (key);
-			    if (kv_entry != null)
-				System.out.println("did read " + key + " with value " + kv_entry.getValue() + " and version " + kv_entry.getVersion());
-			    else
-				System.out.println("failed to read " + key);
-			} catch (NumberFormatException e) {
-			    System.out.println("usage: read key");
-		        }
-		    } else 
-			System.out.println("usage: read key");
+						try {
+							int key =  Integer.parseInt(parameter1);
+							VersionedValue kv_entry  = doRead (key);
+							if (kv_entry != null) {
+								System.out.println("did read " + key + " with value " + kv_entry.getValue() + " and version " + kv_entry.getVersion());
+							} else {
+								System.out.println("failed to read " + key);
+							}
+						} catch (NumberFormatException e) {
+							System.out.println("usage: read key");
+		        		}
+					} else {
+						System.out.println("usage: read key");
+					}
                     break;
-	        case "tx":
-		    System.out.println("tx reading key " + parameter1 + " and key " + parameter2 + " : writting key " + parameter3);
+	        	case "tx":
+		    		System.out.println("tx reading key " + parameter1 + " and key " + parameter2 + " : writting key " + parameter3);
                     if ((parameter1 != null) && (parameter2!=null) && (parameter3!=null)) {
-			try {
-			    int read_key1 =  Integer.parseInt(parameter1);
-			    int read_key2 =  Integer.parseInt(parameter2);
-			    int write_key = Integer.parseInt(parameter3);
-			    if (write_key == 0)
-				System.out.println("key 0 is reserverded for reconfiguration!");
-			    else {
-				int write_value = rnd.nextInt(1000);
-				VersionedValue kv_entry1 = doRead (read_key1);
-				VersionedValue kv_entry2 = doRead (read_key2);
-				if ((kv_entry1!=null) && (kv_entry2!=null)) 
-				    doCommit (read_key1, kv_entry1.getVersion(), read_key2, kv_entry2.getVersion(), write_key, write_value);
-				else
-				    System.out.println("failed to read keys");
-			    }
-			} catch (NumberFormatException e) {
-			    System.out.println("usage: read key");
-		        }
-		    } else 
-                        System.out.println("usage: tx read_key read_key write_key");
-		    break;
-	        case "lenght":
-		    System.out.println("lenght " + parameter1);
+						try {
+							int read_key1 =  Integer.parseInt(parameter1);
+							int read_key2 =  Integer.parseInt(parameter2);
+							int write_key = Integer.parseInt(parameter3);
+							if (write_key == 0) {
+								System.out.println("key 0 is reserverded for reconfiguration!");
+							} else {
+								int write_value = rnd.nextInt(1000);
+								VersionedValue kv_entry1 = doRead (read_key1);
+								VersionedValue kv_entry2 = doRead (read_key2);
+								if ((kv_entry1!=null) && (kv_entry2!=null)) {
+									doCommit (read_key1, kv_entry1.getVersion(), read_key2, kv_entry2.getVersion(), write_key, write_value);
+								} else {
+									System.out.println("failed to read keys");
+								}
+							}
+						} catch (NumberFormatException e) {
+							System.out.println("usage: read key");
+						}
+					} else {
+						System.out.println("usage: tx read_key read_key write_key");
+					}
+					break;
+	        	case "lenght":
+		    		System.out.println("lenght " + parameter1);
                     if (parameter1 != null) {
-			try {
-			     loop_size=  Integer.parseInt(parameter1);
-			} catch (NumberFormatException e) {
-			    System.out.println("usage: lenght loop-lenght");
-		        }
-		    } else 
-			System.out.println("usage: lenght loop-lenght");
-                    break;
-	        case "range":
-		    System.out.println("range " + parameter1);
+						try {
+							loop_size=  Integer.parseInt(parameter1);
+						} catch (NumberFormatException e) {
+							System.out.println("usage: lenght loop-lenght");
+		        		}
+					} else {
+						System.out.println("usage: lenght loop-lenght");
+					}
+					break;
+				case "range":
+					System.out.println("range " + parameter1);
                     if (parameter1 != null) {
-			try {
-			     key_range =  Integer.parseInt(parameter1);
-			} catch (NumberFormatException e) {
-			    System.out.println("usage: range key-range");
-		        }
-		    } else 
-			System.out.println("usage: range key-range");
+						try {
+							key_range =  Integer.parseInt(parameter1);
+						} catch (NumberFormatException e) {
+							System.out.println("usage: range key-range");
+						}
+		    		} else {
+						System.out.println("usage: range key-range");
+					}
                     break;
-	        case "time":
-		    System.out.println("time " + parameter1);
+	        	case "time":
+		    		System.out.println("time " + parameter1);
                     if (parameter1 != null) {
-			try {
-			     sleep_range=  Integer.parseInt(parameter1);
-			} catch (NumberFormatException e) {
-			    System.out.println("usage: time sleep-range");
-		        }
-		    } else 
-			System.out.println("usage: time sleep-range");
+						try {
+							sleep_range=  Integer.parseInt(parameter1);
+						} catch (NumberFormatException e) {
+							System.out.println("usage: time sleep-range");
+						}
+					} else {
+						System.out.println("usage: time sleep-range");
+					}
                     break;
-                 case "loop":
-		     try {
-			doTransactions();
-		     } catch (Exception e) {
-		     }
-		    break;
+                case "loop":
+					try {
+						doTransactions();
+					} catch (Exception e) {}
+					break;
                 case "exit":
                     keep_going = false;
-		    break;
-	        case "":
-		    break;
+		    		break;
+	        	case "":
+		    		break;
                 default:
                     System.out.println("Unknown command: " + mainCommand);
                     break;
             }
-	}
+		}
     }
-    
+
     public void main_loop (String[] args) throws Exception {
-	System.out.println(DadkvsClient.class.getSimpleName());
+		System.out.println(DadkvsClient.class.getSimpleName());
 
-	
-	// receive and print arguments
-	System.out.printf("Received %d arguments%n", args.length);
-	for (int i = 0; i < args.length; i++) {
-	    System.out.printf("arg[%d] = %s%n", i, args[i]);
-	}
+		// receive and print arguments
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
 
-	// check arguments
-	this.parseArgs(args);
-	
-	
-	System.out.println("Client id = " + client_id + " serverhost = " + host + " port = " + port);
+		// check arguments
+		this.parseArgs(args);
 
-	System.out.println("Client key_range = " + key_range + " sleep_range = " + sleep_range + " loop_size = " + loop_size);
+		System.out.println("Client id = " + client_id + " serverhost = " + host + " port = " + port);
+		System.out.println("Client key_range = " + key_range + " sleep_range = " + sleep_range + " loop_size = " + loop_size);
+		System.out.println("Interactive mode = " + interactive_mode);
 
+		// set servers
+		for (int i = 0; i < n_servers; i++) {
+			int target_port = port +i;
+			targets[i] = new String();
+			targets[i] = host + ":" + target_port;
+			System.out.printf("targets[%d] = %s%n", i, targets[i]);
+		}
 
-	System.out.println("Interactive mode = " + interactive_mode);
+		// init the communication stuff
+		this.initComms();
 
-	// set servers
-	for (int i = 0; i < n_servers; i++) {
-	    int target_port = port +i;
-	    targets[i] = new String();
-	    targets[i] = host + ":" + target_port;
-	    System.out.printf("targets[%d] = %s%n", i, targets[i]);
-	}
+		if (interactive_mode == false) {
+			doTransactions();
+		} else {
+			goInteractive();
+		}
 
-	
-	// init the communication stuff
-	this.initComms();
-
-	if (interactive_mode == false)
-	    doTransactions();
-	else
-	    goInteractive();
-	
-       
         System.out.println("closing channels...");
-	terminateComms();
+		terminateComms();
         System.out.println("Exiting...");
-	
     }
-    
-    public static void main(String[] args) throws Exception {
-	System.out.println("Starting...");
-	
-	DadkvsClient client = new DadkvsClient();
 
-	client.main_loop(args);
+    public static void main(String[] args) throws Exception {
+		System.out.println("Starting...");
+
+		DadkvsClient client = new DadkvsClient();
+
+		client.main_loop(args);
     }
 }
 
