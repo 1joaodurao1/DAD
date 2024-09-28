88351f4c394fe40bf586205e9583dc4fe57ea9dc
Miguel Fernandes <miguel.e.fernandes@tecnico.ulisboa.pt>
2024-09-28 15:51:06 +0100

    Remove, now irrelevant, transactionBacklog

--- server/src/main/java/dadkvs/server/DadkvsServerState.java
+++ server/src/main/java/dadkvs/server/DadkvsServerState.java
@@ -19,10 +19,9 @@ public class DadkvsServerState {
     KeyValueStore  store;
     MainLoop       main_loop;
     Thread         main_loop_worker;
-    HashMap<Integer, TransactionRecord> transactionBacklog;
     Queue<Integer>                      orderQueue;
     final int                           n_servers;
-    DadkvsStep1ServiceGrpc.DadkvsStep1ServiceStub[] step1_stubs;
+    DadkvsStep1ServiceGrpc.DadkvsStep1ServiceStub[] async_step1_stubs;
     int nextSeqNumber;
 
 
@@ -34,10 +33,9 @@ public class DadkvsServerState {
         debug_mode = 0;
         store_size = kv_size;
         store = new KeyValueStore(kv_size);
-        transactionBacklog = new HashMap<>();
         orderQueue = new LinkedList<>();
         n_servers = servers;
-        step1_stubs = step1Stubs;
+        async_step1_stubs = step1Stubs;
         nextSeqNumber = 0;
         main_loop = new MainLoop(this);
         main_loop_worker = new Thread (main_loop);
@@ -66,7 +64,7 @@ public class DadkvsServerState {
             for (int i = 0; i < n_servers; i++) {
                 if (i != my_id){
                     CollectorStreamObserver<DadkvsStep1.DefineOrderReply> defineOrder_observer = new CollectorStreamObserver<DadkvsStep1.DefineOrderReply>(defineOrder_collector);
-                    step1_stubs[i].defineOrder(defineOrderRequest.build(), defineOrder_observer);
+                    async_step1_stubs[i].defineOrder(defineOrderRequest.build(), defineOrder_observer);
                 }
 		    }
             defineOrder_collector.waitForTarget(n_servers - 1);  // Wait for responses from all other servers
@@ -75,12 +73,10 @@ public class DadkvsServerState {
 
         synchronized(this){
             boolean result;
-            transactionBacklog.put(reqid, record);
             while (orderQueue.peek() == null || reqid != orderQueue.peek()){ // Wait until you are at the front of the Queue
                 try { wait ();}
                 catch (InterruptedException e) {} // Ignore
             }
-            transactionBacklog.remove(reqid);
             result = this.store.commit(record);
             if(orderQueue.poll() == null) { // Removes item on top
                 System.out.println("ERROR: transaction " + reqid + "executed OUT OF ORDER (queue was empty when it executed)");
