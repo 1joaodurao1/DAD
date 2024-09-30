0bc80770a3a731f537bf53851a5d354ebd9c800a
SalvadorCorreia <salvador.sdc.correia@gmail.com>
2024-09-30 18:52:05 +0100

    client running scrip edited text

--- client/run_clients.sh
+++ client/run_clients.sh
@@ -2,19 +2,19 @@
 
 # Check if the user provided an input
 if [ -z "$1" ]; then
-  echo "Usage: $0 <number_of_servers>"
+  echo "Usage: $0 <number_of_clients>"
   exit 1
 fi
 
-# Get the number of servers to start from the argument
-NUM_SERVERS=$1
+# Get the number of clients to start from the argument
+NUM_clients=$1
 
 # Loop through and start the required number of clients
-for ((i=1; i<=NUM_SERVERS; i++)); do
+for ((i=1; i<=NUM_clients; i++)); do
   mvn exec:java > "logs/client$i.log" 2>&1 &
   echo "Client $i started in the background with PID $!"
 done
 
-# Optionally, wait for all servers to finish (remove this if you don't want to block the terminal)
+# Optionally, wait for all clients to finish (remove this if you don't want to block the terminal)
 wait
 
