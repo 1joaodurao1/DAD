98a8a9e10c1577281006f573e275e947a94187b0
SalvadorCorreia <salvador.sdc.correia@gmail.com>
2024-10-16 17:51:57 +0100

    basic files

---
+++ server/src/main/java/dadkvs/server/DadkvsServerPaxosLeader.java
@@ -0,0 +1,15 @@
+package dadkvs.server;
+
+import dadkvs.DadkvsPaxos;
+import dadkvs.DadkvsPaxosServiceGrpc;
+import dadkvs.util.CollectorStreamObserver;
+import dadkvs.util.GenericResponseCollector;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.HashMap;
+
+import dadkvs.server.DadkvsServerPaxos;
+
+public class DadkvsServerPaxosLeader extends DadkvsServerPaxos {
+
+}
